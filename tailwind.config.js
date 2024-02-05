d40279cf26010e79631521333732479e583f5f8b
matrix-bro <razmhz11@gmail.com>
2024-02-05 11:47:06 +0545

    feat: created imageform for allowing users to share the image and added some simple validation for now

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./account/templates/**/*.html"],
+  content: ["./account/templates/**/*.html", "./app/templates/**/*.html"],
   theme: {
     extend: {},
   },
